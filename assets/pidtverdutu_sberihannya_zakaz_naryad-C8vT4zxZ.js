ae07dd9f610305511062544a6862ddac1642263e
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-31 14:42:10 +0300

    Updates

--- assets/pidtverdutu_sberihannya_zakaz_naryad-C8vT4zxZ.js
+++ assets/pidtverdutu_sberihannya_zakaz_naryad-C8vT4zxZ.js
@@ -1,4 +1,4 @@
-import{g as m,l as u,m as f}from"./main-B57G5v3F.js";import{s as y}from"./supabaseClient-Ckor8WuH.js";const i="save-prompt-modal-create";function p(){const t=document.createElement("div");t.id=i,t.className="modal-create-overlay",t.style.display="none";const n=document.createElement("div");return n.className="modal-content-save",n.innerHTML=`
+import{g as m,l as u,m as f}from"./main-D1NfSqac.js";import{s as y}from"./supabaseClient-SKsD5zbP.js";const i="save-prompt-modal-create";function p(){const t=document.createElement("div");t.id=i,t.className="modal-create-overlay",t.style.display="none";const n=document.createElement("div");return n.className="modal-content-save",n.innerHTML=`
     <p>Створити заказ наряд?</p>
     <div class="save-buttons">
       <button id="save-confirm-create" class="btn-save-confirm">Так</button>
