8dfbb51e97148e9547b9756128c7e56e1fa41205
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-31 11:10:50 +0300

    Оновлення проєкту та підготовка до розгортання на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/STO/", // бо GitHub Pages публікує з підкаталогу
+  base: "'/STOSuprunec/", // бо GitHub Pages публікує з підкаталогу
   plugins: [react()],
   build: {
     rollupOptions: {
