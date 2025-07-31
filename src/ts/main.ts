effbd07d873a7f24ca374fdd35f6888fd530bef5
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-31 13:24:26 +0300

    Очистка репозиторію перед завантаженням нового проєкту

--- src/ts/main.ts
+++
@@ -1,4 +0,0 @@
-import "./roboha/tablucya/tablucya";
-import "./roboha/redahyvatu_klient_machuna/vikno_klient_machuna";
-import "./roboha/inichi_bazu_danux/inchi_bazu_danux";
-import "./roboha/nalachtuvannay/nalachtuvannay";
--- src/ts/vxid/App.tsx
+++
@@ -1,3 +0,0 @@
-export default function App() {
-  return <h2>Вітаю, ви увійшли до додатку!</h2>;
-}
