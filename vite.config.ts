31e9a317cb246d21c331c9454649294e21ac3963
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-31 12:17:31 +0300

    Виправлено базові шляхи для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "'/STOSuprunec/", // бо GitHub Pages публікує з підкаталогу
+  base: '/STOSuprunec/', // правильний шлях до підкаталогу репозиторію
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, "index.html"),
-        main: resolve(__dirname, "main.html"), // цей файл повинен існувати!
+        index: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'main.html'),
       },
     },
   },
 });
+
