712876d6662e1eef8153118d3bb600fc0dbbdd3a
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-31 18:04:45 +0300

    Updates

--- assets/index-CncfYwIT.js
+++ assets/index-CncfYwIT.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-SKsD5zbP.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-47xAA36K.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *
